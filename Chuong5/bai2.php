98cb5d5f01295e8d894cba678604f656fb2782c8
NDuy2005 <nduy2005zz@gmail.com>
2026-03-13 13:54:36 +0700

    nop chuong5

---
+++ Chuong5/bai2.php
@@ -0,0 +1,11 @@
+<?php
+$hoten = "Ngô Nhật Duy";
+$ngaysinh = "11/06/2005";
+$lop = "17DCNTT2";
+$diem = 8.9;
+
+echo "Họ tên: $hoten <br>";
+echo "Ngày sinh: $ngaysinh <br>";
+echo "Lớp: $lop <br>";
+echo "Điểm: $diem";
+?>
